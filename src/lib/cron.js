91d83972aca5302baa7eb2d5978fe0c25d4cc6d0
SRB <souhardaroybarman05@gmail.com>
2025-03-27 22:24:38 +0530

    Add cron job to send periodic HTTPS requests to API

---
+++ src/lib/cron.js
@@ -0,0 +1,13 @@
+import cron from "cron";
+import https from "https";
+import {API_URL} from "backend/.env";
+
+
+const job = new cron.CronJob("*/14 * * * *", function (){
+    https
+        .get(process.env.API_URL,(res) =>{
+    if(res.statusCode===200) console.log("Get request sent successfully");
+    else console.log("Get request failed",res.statusCode);
+    }).on("error",(e) => console.error("Error while sending request",e));
+});
+export default job;
