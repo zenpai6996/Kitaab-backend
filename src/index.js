c98b3ab11c870c67e9208593f6ea356b1f365a38
SRB <souhardaroybarman05@gmail.com>
2025-03-27 23:23:09 +0530

    merge conflicts are hell :(

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import "dotenv/config";
+import job from "./lib/cron.js";
 
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 
 import {connectDB} from "./lib/db.js";
 
+job.start();
 const app = express();
 const port = process.env.PORT ;
 
