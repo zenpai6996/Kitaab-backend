68fe537363a076941b992e92169e592539cedf57
SRB <souhardaroybarman05@gmail.com>
2025-03-30 00:26:20 +0530

    Add book creation endpoint with image upload and validation

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -7,6 +7,39 @@ const router = express.Router();
 
 //create book endpoint
 router.post("/",protectRoute ,async (req,res) =>{
+    try{
+        const {title,caption,image,rating} =req.body;
+
+        if(!title || !caption || !image || !rating){
+            return res.status(400).json({message:"Please provide all fields"});
+        }
+
+        //upload image to cloudinary
+        const uploadResponse = await cloudinary.uploader.upload(image);
+        const imageUrl = uploadResponse.secure_url;
+
+        //save to database
+        const newBook = new Book({
+            title,
+            caption,
+            image:imageUrl,
+            rating,
+            user:req.user._id
+        })
+
+        await newBook.save();
+
+        res.status(201).json(newBook)
+
+    }catch(error){
+        console.log("Error creating book",error);
+        res.status(500).json({message:error.message});
+    }
+})
+
+//recommended books endpoint
+//pagination => infinite scroll
+router.get("/",protectRoute,async(req,res) =>{
     try {
         const limit = parseInt(req.query.limit) || 5;
         const lastBookId = req.query.lastBookId; // Get last book's ID from query
@@ -23,37 +56,6 @@ router.post("/",protectRoute ,async (req,res) =>{
         console.log("Error fetching books", error);
         res.status(500).json({ message: error.message });
     }
-})
-
-//recommended books endpoint
-//pagination => infinite scroll
-router.get("/",protectRoute,async(req,res) =>{
-   //example call:
-   //const response = await fetch("http://localhost:3000/api/books?page=1&limit=5);
-    try{
-
-    const page = req.query.page || 1;
-    const limit = req.query.limit || 5;
-    const skip = ( page - 1 )*limit;
-
-    const books = await Book.find()
-        .sort({createdAt:-1})
-        .skip(skip)
-        .limit(limit)
-        .populate("user","username profileImage");//descendign order
-
-    const total = await Book.countDocuments();
-    res.send({
-        books,
-        currentPage:page,
-        total,
-        totalPages:Math.ceil(total/limit),
-    });
-
-   } catch (error){
-       console.log("Error fetching books",error);
-         res.status(500).json({message:error.message});
-   }
 });
 
 //user recommendations
