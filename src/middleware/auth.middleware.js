aa15c43bcae5f5470c65e5ece6e69658ee92ee49
SRB <souhardaroybarman05@gmail.com>
2025-03-29 18:06:21 +0530

    Fix token extraction in auth middleware and update API endpoints to use constants

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -5,7 +5,7 @@ import User from "../models/User.js";
 
 const protectRoute = async (req,res,next) =>{
     try{
-       const token = req.header("Authorization").replace("Bearer","");
+       const token = req.header("Authorization").replace("Bearer ","");
        if(!token) return res.status(401).json({message:"No authentication token, access denied"});
 
        //verify token
