735af2949399da974a5400650bc22ca46d8ce0c5
SRB <souhardaroybarman05@gmail.com>
2025-03-29 21:45:48 +0530

    Fix typo in response object for totalBooks in bookRoutes.js

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -58,8 +58,8 @@ router.get("/",protectRoute,async(req,res) =>{
     res.send({
         books,
         currentPage:page,
-        totalBooks:total,
-        totalPages:Math.ceil(totalBooks/limit)
+        totalbooks:total,
+        totalPages:Math.ceil(total/limit)
     });
 
    } catch (error){
